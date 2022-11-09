44ae2dcbce7bef1b7fce441e6e16a5a387827475
Md. Junaidul Islam <info.junaidulislam@gmail.com>
2022-11-09 23:43:42 +0600

    footer updated

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -4,6 +4,7 @@ import { FaFacebook, FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
 import { FcHome, FcPhone } from "react-icons/fc";
 import { FiMail } from "react-icons/fi";
 import { TbHeartRateMonitor } from "react-icons/tb";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
@@ -36,59 +37,13 @@ const Footer = () => {
                                 <TbHeartRateMonitor size={50} /> FixPC
                             </h6>
                             <p>
-                                I'm always here to help. Fix your dream PC within 24 hrs. You can get your desired services according to your needs with free consultancy.
+                                I'm always here to help. Fix your technical
+                                issues within 24 hrs. You can get your desired
+                                services according to your needs with free
+                                consultancy.
                             </p>
                         </div>
-                        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                            <h6 className="text-uppercase fw-bold mb-4">
-                                Services
-                            </h6>
-                            <p>
-                                <a href="#!" className="text-reset">
-                                    Angular
-                                </a>
-                            </p>
-                            <p>
-                                <a href="#!" className="text-reset">
-                                    React
-                                </a>
-                            </p>
-                            <p>
-                                <a href="#!" className="text-reset">
-                                    Vue
-                                </a>
-                            </p>
-                            <p>
-                                <a href="#!" className="text-reset">
-                                    Laravel
-                                </a>
-                            </p>
-                        </div>
-                        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                            <h6 className="text-uppercase fw-bold mb-4">
-                                Useful links
-                            </h6>
-                            <p>
-                                <a href="#!" className="text-reset">
-                                    Pricing
-                                </a>
-                            </p>
-                            <p>
-                                <a href="#!" className="text-reset">
-                                    Settings
-                                </a>
-                            </p>
-                            <p>
-                                <a href="#!" className="text-reset">
-                                    Orders
-                                </a>
-                            </p>
-                            <p>
-                                <a href="#!" className="text-reset">
-                                    Help
-                                </a>
-                            </p>
-                        </div>
+
                         <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                             <h6 className="text-uppercase fw-bold mb-4">
                                 Contact
@@ -100,19 +55,19 @@ const Footer = () => {
                                 <FiMail /> info.junaidulislam@gmail.com
                             </p>
                             <p>
-                                <FcPhone /> + 01 234 567 88
+                                <FcPhone /> +880 123 456 7890
                             </p>
                         </div>
                     </div>
                 </div>
                 <div className="text-center p-4">
-                    © 2022 Copyright:
-                    <a
-                        className="text-reset fw-bold"
-                        href="https://fixpc-fe9de.web.app/"
+                    © 2022 Copyright{" "}
+                    <Link
+                        to="/"
+                        className="text-reset fw-bold text-decoration-none"
                     >
                         FixPC
-                    </a>
+                    </Link>
                 </div>
             </Container>
         </div>
