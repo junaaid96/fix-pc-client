7fe6a909b61e5921033104a24ef3085de3bc1784
Md. Junaidul Islam <info.junaidulislam@gmail.com>
2022-11-10 00:09:22 +0600

    login page updated

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,49 +1,69 @@
 import React from "react";
-import { Button, Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import { Form, Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
+import img from "../../Assets/login.jpg";
 
 const Login = () => {
     return (
         <Container className="bg-light p-5 my-5 border shadow">
-            <Form className="w-50 m-auto">
-                <div className="form-outline mb-4">
-                    <label className="form-label" for="formEmail">
-                        Email address
-                    </label>
-                    <input
-                        type="email"
-                        id="formEmail"
-                        className="form-control"
-                        placeholder="Email"
-                    />
-                </div>
-                <div className="form-outline mb-4">
-                    <label className="form-label" for="formPassword">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        id="formPassword"
-                        className="form-control"
-                        placeholder="Password"
-                    />
-                    <div className="my-2"><a href="#!">Forgot password?</a></div>
-                </div>
+            <Row className="gap-5">
+                <Col lg="5">
+                    <Form>
+                        <div className="form-outline mb-4">
+                            <label className="form-label" for="formEmail">
+                                Email address
+                            </label>
+                            <input
+                                type="email"
+                                id="formEmail"
+                                className="form-control"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="form-outline mb-4">
+                            <label className="form-label" for="formPassword">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="formPassword"
+                                className="form-control"
+                                placeholder="Password"
+                            />
+                            <div className="my-2">
+                                <a href="#!">Forgot password?</a>
+                            </div>
+                        </div>
 
-                <button type="submit" className="btn btn-dark mb-4">
-                    Sign in
-                </button>
+                        <button type="submit" className="btn btn-dark mb-4">
+                            Sign in
+                        </button>
 
-                <div className="text-center">
-                    <p>
-                        New here? <Link to="/registration" className="text-decoration-none fw-bold text-dark">Sign Up</Link>
-                    </p>
-                    <Button type="submit" variant="light" className="border">
-                        <FcGoogle /> Sign in with Google
-                    </Button>
-                </div>
-            </Form>
+                        <div className="text-center">
+                            <p>
+                                New here?{" "}
+                                <Link
+                                    to="/registration"
+                                    className="text-decoration-none fw-bold text-dark"
+                                >
+                                    Sign Up
+                                </Link>
+                            </p>
+                            <Button
+                                type="submit"
+                                variant="light"
+                                className="border"
+                            >
+                                <FcGoogle /> Sign in with Google
+                            </Button>
+                        </div>
+                    </Form>
+                </Col>
+                <Col lg="5">
+                    <img fluid src={img} alt="login" className="w-100"/>
+                </Col>
+            </Row>
         </Container>
     );
 };
