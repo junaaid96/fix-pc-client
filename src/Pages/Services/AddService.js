83e424420ad3bcdcf513988c0da52f1014b44a5d
Md. Junaidul Islam <info.junaidulislam@gmail.com>
2022-11-10 15:38:09 +0600

    add service form added

--- src/Pages/Services/AddService.js
+++ src/Pages/Services/AddService.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
 
 const AddService = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch("https://fix-pc-server.vercel.app/services")
+            .then((res) => res.json())
+            .then((data) => setServices(data));
+    }, []);
+
     return (
-        <div>
-            add service
-        </div>
+        <Container className="bg-light border shadow my-5 p-5">
+            <h2 className="mb-5">Services: {services.length}</h2>
+            <Form>
+                <Form.Group className="mb-3" controlId="titleForm.ControlInput">
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="title"
+                        placeholder="title"
+                    />
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="pictureForm.ControlInput"
+                >
+                    <Form.Label>Picture</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="picture"
+                        placeholder="picture URL"
+                    />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="priceForm.ControlInput">
+                    <Form.Label>Price</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="price"
+                        placeholder="price"
+                    />
+                </Form.Group>
+                <Form.Group
+                    className="mb-3"
+                    controlId="descriptionForm.ControlInput"
+                >
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="description"
+                        placeholder="description"
+                    />
+                </Form.Group>
+                <Button className="my-3" variant="dark">Add Service</Button>
+            </Form>
+        </Container>
     );
 };
 
--- src/Pages/Services/ServiceDetails.js
+++ src/Pages/Services/ServiceDetails.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Contexts/AuthProvider";
 
@@ -44,26 +44,51 @@ const ServiceDetails = () => {
             <Row className="my-5">
                 <h2 className="mb-5">Reviews: {reviews.length}</h2>
                 {user?.email ? (
-                    <Form>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="emailForm.ControlInput"
-                        >
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder={`${user.email}`}
-                                readOnly
-                            />
-                        </Form.Group>
-                        <Form.Group
-                            className="mb-3"
-                            controlId="reviewForm.ControlTextarea"
-                        >
-                            <Form.Label>Review</Form.Label>
-                            <Form.Control as="textarea" rows={3} />
-                        </Form.Group>
-                    </Form>
+                    <>
+                        <Form>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="nameForm.ControlInput"
+                            >
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control
+                                    type="text"
+                                    name="name"
+                                    placeholder={`${user.displayName}`}
+                                    readOnly
+                                />
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="emailForm.ControlInput"
+                            >
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    name="email"
+                                    placeholder={`${user.email}`}
+                                    readOnly
+                                />
+                            </Form.Group>
+                            <Form.Group
+                                className="mb-3"
+                                controlId="reviewForm.ControlTextarea"
+                            >
+                                <Form.Label>Review</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                            <Button variant="dark">Post</Button>
+                        </Form>
+                        <h2 className="my-5">Earlier Reviews</h2>
+                        {reviews.map((review) => (
+                            <>
+                                <h5>{review.name}</h5>
+                                <p className="bg-white p-3 rounded-3">
+                                    {review.text}
+                                </p>
+                            </>
+                        ))}
+                    </>
                 ) : (
                     <p className="text-dark">
                         Please{" "}
